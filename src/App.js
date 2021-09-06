b0bdfc4245c8b10b5ff0683269243d08e13f6a0c
Suryansh Srivastava <suryanshsriv1@gmail.com>
2021-09-07 00:24:47 +0530

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return ( <
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <
+        h1 > Welcome to React Parcel Micro App! < /h1> <
+        p > Hard to get more minimal than this React app. < /p> <
+        />
+    );
 }
 
 export default App;
